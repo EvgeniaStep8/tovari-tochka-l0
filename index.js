dc94fd0dd65b541623319d7b7a078a0ef5781baa
Evgenia Stepanova <evgeniast85es@yandex.ru>
2023-08-15 12:15:18 +0300

    add listener on side-bar edit button

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import Popup from "./components/Popup.js";
-import { editPayButton, editDeliveryButton } from "./utils/constants.js";
+import {
+  editPayButton,
+  sideBarEditPayButton,
+  editDeliveryButton,
+  sideBarEditDeliveryButton,
+} from "./utils/constants.js";
 
 const payPopup = new Popup("#pay-popup", ".popup__close-button");
 const deliveryPopup = new Popup("#delivery-popup", ".popup__close-button");
@@ -8,4 +13,6 @@ payPopup.setEventListeners();
 deliveryPopup.setEventListeners();
 
 editPayButton.addEventListener("click", () => payPopup.open());
+sideBarEditPayButton.addEventListener("click", () => payPopup.open());
 editDeliveryButton.addEventListener("click", () => deliveryPopup.open());
+sideBarEditDeliveryButton.addEventListener("click", () => deliveryPopup.open());
--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,13 @@
 const editPayButton = document.querySelector("#edit-pay");
+const sideBarEditPayButton = document.querySelector("#side-bar-edit-pay");
 const editDeliveryButton = document.querySelector("#edit-delivery");
+const sideBarEditDeliveryButton = document.querySelector(
+  "#side-bar-edit-delivery"
+);
 
-export { editPayButton, editDeliveryButton };
+export {
+  editPayButton,
+  sideBarEditPayButton,
+  editDeliveryButton,
+  sideBarEditDeliveryButton,
+};
